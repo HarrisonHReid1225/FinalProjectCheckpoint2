0778153b3dfbaa2c0e0ce1be2b60c46825a83097
HarrisonHReid <hhr0011@auburn.edu>
2026-07-06 00:13:06 -0400

    Initial commit

---
+++ Assets/Scripts/ResourceCounter.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class ResourceCounter : MonoBehaviour
+{
+    public TextMeshProUGUI appleText;
+    public TextMeshProUGUI oreText;
+    public TextMeshProUGUI pearText;
+
+    private int apples;
+    private int ores;
+    private int pears;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        appleText.text = "Apples: " + apples;
+        oreText.text = "Ores: " + ores;
+        pearText.text = "Pears: " + pears;
+    }
+
+    public void AddResource(string resourceName, int amount)
+    {
+        if (resourceName == "Apple")
+        {
+            apples += amount;
+        }
+        else if (resourceName == "Ore")
+        {
+            ores += amount;
+        }
+        else if (resourceName == "Pear")
+        {
+            pears += amount;
+        }
+
+        UpdateUI();
+    }
+}
